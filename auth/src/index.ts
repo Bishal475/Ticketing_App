39b494019db0ae9f42a72cc2a903c449dd052750
Bishal Tiwari <1605087@gmail.com>
2024-12-23 10:25:16 +0530

    Initial Commit 1

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -22,7 +22,7 @@ app.use(errorHandler);
 const start= async()=>{
     try{
         await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB WOW");
     }catch(err){
         console.error(err);
     }
